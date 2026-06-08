09b6f5c071ffa140f9c1897b45ce8db1ca7c7e08
Huy Luong Duc <huyluongme.cs@gmail.com>
2026-06-08 18:52:24 +0000

    add chapter 1, section 1.4

--- chapter-1-a-tutorial-introduction/1.4-symbolic-constants/temperature_converter_v4.c
+++ chapter-1-a-tutorial-introduction/1.4-symbolic-constants/temperature_converter_v4.c
@@ -1,5 +1,5 @@
 /*
- * File name:   temperature_converter_v3.c
+ * File name:   temperature_converter_v4.c
  * Author:      Huy Luong Duc <huyluongme.cs@gmail.com>
  * Date:        Jun 9, 2026
  *
@@ -11,13 +11,17 @@
 
 #include <stdio.h>
 
+#define LOWER   0   /* lower limit of table */
+#define UPPER   300 /* upper limit */
+#define STEP    20  /* step size */
+
 /* printf Fahrenheit-Celsius table */
 
 int main()
 {
     int fahr;
 
-    for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
         printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
 
     return 0;
